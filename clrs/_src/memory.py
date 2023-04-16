3eb1342cb315e9eab20e5e8ab86bf47b4ba8c6b5
Rishabh Jain <rj412@cam.ac.uk>
2023-04-16 16:25:12 +0100

    Added pqv2 with sig and proper heads.

--- clrs/_src/memory.py
+++ clrs/_src/memory.py
@@ -1975,6 +1975,168 @@ class PriorityQueueV2_Sigmoid_atv2(MemoryModule):
         )
 
 
+class PriorityQueueV2_Sigmoid_ProperHeads(MemoryModule):
+    def __init__(
+        self,
+        output_size: int,
+        embedding_size: int,
+        memory_size: int,
+        nb_heads: int,
+        aggregation_technique: str = "max",
+        message_per_head: bool = False,
+        name: str = "neural_priority_queue_v2",
+    ):
+        super().__init__(name=name)
+        self._output_size = output_size
+        self._embedding_size = embedding_size
+        self._memory_size = memory_size
+        self.nb_heads = nb_heads
+        self.aggregation_technique = aggregation_technique
+        self.message_per_head = message_per_head
+
+    def initial_state(self, batch_size: int, **kwargs) -> PriorityQueueStateV2:
+        memory_values = jnp.zeros((batch_size, self._memory_size, self._embedding_size))
+        read_strengths = jnp.zeros((batch_size, self._memory_size))
+        write_mask = jax.nn.one_hot(
+            [0] * batch_size,
+            num_classes=self._memory_size,
+            dtype=jnp.float32,
+        )  # [B, S]
+        return PriorityQueueStateV2(
+            memory_values=memory_values,
+            read_strengths=read_strengths,
+            write_mask=write_mask,
+        )
+
+    def __call__(
+        self, z: _Array, prev_state: PriorityQueueStateV2, **kwargs
+    ) -> Tuple[_Array, PriorityQueueStateV2]:
+        # z.shape: [B, N, F]
+        batch_size, nb_nodes, nb_z_fts = z.shape
+        if prev_state is None:
+            prev_state = self.initial_state(batch_size=batch_size)
+
+        push_proj = hk.Linear(output_size=1)
+        pop_proj = hk.Linear(output_size=1)
+        value_proj = hk.Linear(output_size=self._embedding_size)
+        output_proj = hk.Linear(output_size=self._output_size)
+
+        push_strengths = push_proj(z)  # [B, N, 1]
+        push_strengths = jnp.sum(push_strengths, axis=1)  # [B, 1]
+        push_strengths = jax.nn.sigmoid(push_strengths)  # [B, 1]
+
+        pop_strengths = pop_proj(z)  # [B, N, 1]
+        pop_strengths = jax.nn.sigmoid(pop_strengths)  # [B, N, 1]
+        node_wise_pop_strengths = pop_strengths  # [B, N, 1]
+
+        write_values = value_proj(z.reshape(batch_size * nb_nodes, nb_z_fts))
+        write_values = jnp.reshape(
+            write_values, (batch_size, nb_nodes, self._embedding_size)
+        )  # [B, N, F]
+        write_values = jnp.sum(write_values, axis=1)
+        write_values = jnp.tanh(write_values)  # [B, F']
+
+        a_1 = hk.Linear(self.nb_heads)
+        a_2 = hk.Linear(self.nb_heads)
+
+        att_1 = jnp.expand_dims(a_1(z), axis=-1)  # [B, N, H, 1]
+        att_2 = jnp.expand_dims(a_2(prev_state.memory_values), axis=-1)  # [B, S, H, 1]
+
+        # [B, H, N, 1] + [B, H, 1, S]  = [B, H, N, S]
+        logits = jnp.transpose(att_1, (0, 2, 1, 3)) + jnp.transpose(att_2, (0, 2, 3, 1))
+
+        # Masking out the unwritten memory cells
+        unwritten_mask = prev_state.read_strengths < SMALL_NUMBER
+        bias_mat = unwritten_mask * -1e9  # [B, S]
+        bias_mat = jnp.expand_dims(bias_mat, 1)  # [B, 1, S]
+        bias_mat = jnp.expand_dims(bias_mat, 1)  # [B, 1, 1, S]
+        bias_mat = jnp.tile(bias_mat, (1, self.nb_heads, nb_nodes, 1))  # [B, H, N, S]
+
+        coefs = jax.nn.softmax(
+            jax.nn.leaky_relu(logits) + bias_mat, axis=-1
+        )  # [B, H, N, S]
+
+        new_read_strengths = prev_state.read_strengths
+        # Calculating the proportion of each element popped
+        # (and their contribute to individual outputs)
+        if self.aggregation_technique == "max":
+            max_att_idx = jnp.argmax(
+                coefs,
+                axis=-1,
+            )  # [B, H, N]
+
+            one_hot_max_att = jax.nn.one_hot(
+                max_att_idx,
+                num_classes=self._memory_size,
+            )  # [B, H, N, S]
+            pop_requested = (
+                jnp.expand_dims(node_wise_pop_strengths, axis=1)
+                * one_hot_max_att
+                / self.nb_heads
+            )  # [B, H, N, S]
+        elif self.aggregation_technique == "weighted":
+            pop_requested = (
+                jnp.expand_dims(node_wise_pop_strengths, axis=1) * coefs / self.nb_heads
+            )  # [B, H, N, S]
+        else:
+            raise ValueError(
+                "Unknown memory aggregation technique " + self.aggregation_technique
+            )
+        total_pop_requested = jnp.sum(
+            pop_requested,
+            axis=1,
+        )  # [B, N, S]
+        total_pop_requested = jnp.sum(
+            total_pop_requested,
+            axis=1,
+        )  # [B, S]
+        recp_total_pop_requested = 1 / (total_pop_requested + SMALL_NUMBER)  # [B, S]
+        pop_proportions = pop_requested * jnp.expand_dims(
+            recp_total_pop_requested, axis=[1, 2]
+        )  # [B, H, N, S]
+
+        pop_given = jnp.minimum(
+            prev_state.read_strengths, total_pop_requested
+        )  # [B, S]
+        output_coefs = pop_proportions * jnp.expand_dims(
+            pop_given, axis=[1, 2]
+        )  # [B, H, N, S]
+        output = jnp.expand_dims(output_coefs, axis=-1) * jnp.expand_dims(
+            prev_state.memory_values, axis=[1, 2]
+        )  # [B, H, N, S, F']
+        output = jnp.sum(output, axis=3)  # [B, H, N, F']
+        new_read_strengths -= pop_given
+        if self.message_per_head:
+            output = output_proj(output)  # [B, H, N, O]
+        else:
+            output = jnp.transpose(output, [0, 2, 1, 3])
+            output = jnp.reshape(
+                output, (batch_size, nb_nodes, self.nb_heads * self._embedding_size)
+            )
+            output = output_proj(output)
+
+        new_memory_values = prev_state.memory_values + jnp.expand_dims(
+            write_values, axis=1
+        ) * jnp.expand_dims(prev_state.write_mask, axis=2)
+        unwritten_mask = new_read_strengths < SMALL_NUMBER
+        new_read_strengths *= 1 - unwritten_mask
+        new_read_strengths = new_read_strengths + push_strengths * prev_state.write_mask
+
+        unwritten_mask = new_read_strengths < SMALL_NUMBER
+        get_next_write_places = jax.vmap(lambda x: jnp.flatnonzero(x, size=1), 0, 0)
+        next_write_places = get_next_write_places(unwritten_mask)  # [B, 1]
+        next_write_places = jnp.squeeze(next_write_places, axis=-1)  # [B]
+        new_write_mask = jax.nn.one_hot(
+            next_write_places, num_classes=self._memory_size
+        )  # [B, S]
+
+        return output, PriorityQueueStateV2(
+            memory_values=new_memory_values,
+            write_mask=new_write_mask,
+            read_strengths=new_read_strengths,
+        )
+
+
 def memory_factory(
     memory_module: str,
     memory_module_args: Dict[str, Any],
@@ -2197,6 +2359,18 @@ def memory_factory(
             nb_heads=memory_module_args["nb_heads"],
             aggregation_technique=memory_module_args["aggregation_technique"],
         )
+    elif memory_module == "priority_queue_v2_sig_ph":
+        return PriorityQueueV2_Sigmoid_ProperHeads(
+            output_size=message_dimension
+            if memory_module_args["direct_output"]
+            else feature_dimension,
+            embedding_size=feature_dimension
+            if memory_module_args["embedding_size"] is None
+            else memory_module_args["embedding_size"],
+            memory_size=memory_module_args["memory_size"],
+            nb_heads=memory_module_args["nb_heads"],
+            aggregation_technique=memory_module_args["aggregation_technique"],
+        )
     else:
         raise ValueError("Unexpected processor memory kind " + memory_module)
 
@@ -2213,7 +2387,7 @@ def update_using_memory(
     message_enc: Optional[hk.Module] = None,
     edge_msgs: Optional[_Array] = None,
     graph_msgs: Optional[_Array] = None,
-) -> Tuple[_Array, _Array]:
+) -> Tuple[_Array, _Array, MemoryState, MemoryModule]:
     if isinstance(memory_module, str):
         memory_module = memory_factory(
             memory_module=memory_module,
